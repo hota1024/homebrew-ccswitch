c57e8c913dc2b04acc79fe28fa5258a27d49ef4d
HotakaOkumoto <okumoto@ptyhard.co.jp>
2025-07-09 01:59:05 +0900

    v1.0.2

--- Formula/ccswitch.rb
+++ Formula/ccswitch.rb
@@ -1,13 +1,13 @@
 class Ccswitch < Formula
   desc "Claude Code profile switcher"
   homepage "https://github.com/hota1024/ccswitch"
-  url "https://github.com/hota1024/ccswitch/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "753b95ec276884109f56f5219cdba859d45d7c5702b0283f6d31efc8e3a35a51"
+  url "https://github.com/hota1024/ccswitch/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "c3f5c337e976833365ed2374abae87eb62e7b39a7a421af071b585777de30a15"
 
   depends_on "rust" => :build
 
   def install
+    bin.install "ccswitch"
     system "cargo", "install", "--root", prefix, "--path", "."
-    bin.install "ccswitch.sh"
   end
 end
