1823e5e96041b6c704e67515616d152fe7d2b52e
HotakaOkumoto <okumoto@ptyhard.co.jp>
2025-07-09 02:35:36 +0900

    v1.0.5

--- Formula/ccswitch.rb
+++ Formula/ccswitch.rb
@@ -1,13 +1,23 @@
 class Ccswitch < Formula
   desc "Claude Code profile switcher"
   homepage "https://github.com/hota1024/ccswitch"
-  url "https://github.com/hota1024/ccswitch/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "25ddd997c050e2d73b4a746c4545c2fd354e9b0e42b292b9a73b46b65b5c7006"
+  url "https://github.com/hota1024/ccswitch/archive/refs/tags/v1.0.5.tar.gz"
+  sha256 "eabc715ac7551c848217a2543652204ba2f207f1304f1ee64ed62999519a7b4d"
 
   depends_on "rust" => :build
 
   def install
-    bin.install "ccswitch"
+    bin.install "ccswitch_fn"
     system "cargo", "install", "--root", prefix, "--path", "."
+    puts "Installation completed successfully."
+  end
+
+  def caveats
+    <<~EOS
+      Please add the following script to your shell configuration file(e.g. ~/.bashrc, ~/.zshrc):
+      ```shell
+      source #{opt_bin}/ccswitch_fn
+      ```
+    EOS
   end
 end
